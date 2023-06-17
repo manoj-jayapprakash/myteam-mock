0da52539b14bbbf8581ca5caa70b5f3c1e06326a
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2023-06-17 15:25:32 +0530

    Modified prettier settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
-export function App() {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-  );
-}
-
-export function WrappedApp() {
-  return (
-    <HashRouter>
-      <App />
-    </HashRouter>
   );
 }
